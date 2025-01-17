b56dec1dddd081134b99bad73800409f70563149
Jiaqi Fu(Jack) <fujf1@student.unimelb.edu.au>
2025-01-18 00:42:22 +1100

    Adding slash commands

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from email.policy import default
 
 import discord
 from dotenv import load_dotenv
+from discord import app_commands
 from discord.ext import commands
 
 load_dotenv()
@@ -12,15 +13,19 @@ TOKEN = os.getenv('BOT_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(f"Failed to sync commands: {e}")
 
-@bot.command()
-async def hello(context):
-    await context.send(f'Hello, {context.author}!')
-
+@bot.tree.command(name='ping')
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
 
 bot.run(TOKEN)
